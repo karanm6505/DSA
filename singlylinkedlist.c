b758a1f3fad8b357a8817b6d477849899182c4cd
Karan <karanm6505@gmail.com>
2024-09-22 01:21:46 +0530

    Committed

--- singlylinkedlist.c
+++ singlylinkedlist.c
@@ -5,3 +5,16 @@ typedef struct node
     int data;
     struct node* next;
 }Node;
+Node* createNode(int data)
+{
+    Node* newnode = malloc(sizeof(Node));
+    newnode->data = data;
+    newnode->next = NULL;
+    return newnode;
+}
+
+int main()
+{
+    Node* head = malloc(sizeof(Node));
+    head = NULL;
+}
